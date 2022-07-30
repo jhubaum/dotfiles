f5753d8c120b684105dffe79d16045f40388a953
Johannes Huwald <jhuwald@soundhound.com>
2022-07-30 13:43:36 +0200

    Set transparency for non-current windows as well

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,7 +68,8 @@ require('packer').startup(function()
 
 vim.g.tokyonight_style = "night"
 vim.cmd "colorscheme tokyonight"
-vim.cmd "highlight normal guibg=000000"
+vim.cmd "highlight Normal guibg=000000"
+vim.cmd "highlight NormalNC guibg=000000"
 
 local configs = require'nvim-treesitter.configs'
 configs.setup {
