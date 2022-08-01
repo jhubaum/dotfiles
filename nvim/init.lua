101a3bfd6930c7e26c56b609a03dced3d6212c36
Johannes Huwald <johannes.huwald@gmail.com>
2022-08-01 11:32:44 +0200

    Split config into submodules
    Also commented some keybindings I copied from a tutorial I used for the initial setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,70 +1,6 @@
-vim.o.termguicolors = true
-vim.o.syntax = 'on'
-vim.o.errorbells = false
-vim.o.smartcase = true
-vim.o.showmode = false
-vim.bo.swapfile = false
-vim.o.backup = false
-vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
-vim.o.undofile = true
-vim.o.incsearch = true
-vim.o.hidden = true
-vim.o.completeopt='menuone,noinsert,noselect'
-vim.bo.autoindent = true
-vim.bo.smartindent = true
-
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.signcolumn = 'yes'
-vim.wo.wrap = false
-
-vim.g.mapleader = ' '
--- Key mappings
-local key_mapper = function(mode, key, result)
-  vim.api.nvim_set_keymap(
-    mode,
-    key,
-    result,
-    {noremap = true, silent = true}
-  )
-end
-
-key_mapper('', '<up>', '<nop>')
-key_mapper('', '<down>', '<nop>')
-key_mapper('', '<left>', '<nop>')
-key_mapper('', '<right>', '<nop>')
-
---- startup and add configure plugins
-require('packer').startup(function()
-  local use = use
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- Lsp
-  use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/completion-nvim'
-  use 'anott03/nvim-lspinstall'
-
-  -- Fuzzy finder
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-lua/telescope.nvim'
-  use 'jremmen/vim-ripgrep'
-
-  use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'sheerun/vim-polyglot'
-
-  use 'folke/tokyonight.nvim'
-
-  end
-)
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
 
 vim.g.tokyonight_style = "night"
 vim.cmd "colorscheme tokyonight"
@@ -100,21 +36,3 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
--- Lsp
-key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
-key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
-key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
-key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
-key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
-key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
-key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
-key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
-key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
-key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
-key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
-
--- Fuzzy Search
-key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
-key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
-key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
-key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,37 @@
+local keymap = vim.keymap.set
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- disable arrow keys for navigation
+keymap("", "<up>", "<nop>", opts)
+keymap("", "<down>", "<nop>", opts)
+keymap("", "<left>", "<nop>", opts)
+keymap("", "<right>", "<nop>", opts)
+
+-- Skip these options for now, since I don't like the key bindings
+--[[
+-- Lsp
+key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
+key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
+key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+
+-- Fuzzy Search
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+]]
