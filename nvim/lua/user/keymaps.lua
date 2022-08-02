b77b93b6e0c2e52206d7ca14f8dd39f39d77a915
Johannes Huwald <johannes.huwald@gmail.com>
2022-08-02 10:00:05 +0200

    Start replacing commented key mappings

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -14,15 +14,21 @@ keymap("", "<down>", "<nop>", opts)
 keymap("", "<left>", "<nop>", opts)
 keymap("", "<right>", "<nop>", opts)
 
+-- keymappings around buffers
+keymap("", "<leader>bb", ':lua require"telescope.builtin".buffers()<CR>', opts)
+
+-- keymappings around code and lsp interactions
+keymap("", "<leader>cd", ':lua vim.lsp.buf.definition()<CR>')
+keymap("", "<leader>cr", ':lua vim.lsp.buf.references()<CR>')
+
+-- keymappings for project and file navigation
+keymap('', '<leader>pf', ':lua require"telescope.builtin".find_files()<CR>')
+
 -- Skip these options for now, since I don't like the key bindings
 --[[
 -- Lsp
-key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
-key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
-key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
 key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
 key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
-key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
 key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
 key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
 key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
@@ -30,8 +36,6 @@ key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
 key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
 
 -- Fuzzy Search
-key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
 key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
 key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
-key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
 ]]
