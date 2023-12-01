34a4330a574d998ee4ef262c7a5fb193184935eb
Johannes Huwald <jhuwald@soundhound.com>
2023-12-01 16:59:36 +0100

    [nvim] change indent to four spaces

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -14,9 +14,9 @@ vim.bo.autoindent = true
 vim.bo.smartindent = true
 vim.o.wildmode = 'list:longest'
 
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.wo.number = true
 vim.wo.relativenumber = true
