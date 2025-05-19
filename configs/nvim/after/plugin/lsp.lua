2e9cf8de0bc3269f90f120442080251f0e16dbdb
Johannes Huwald <johannes.huwald@gmail.com>
2025-05-19 10:30:58 +0200

    [nvim] Migrate lsp config to newer nvim version

--- configs/nvim/after/plugin/lsp.lua
+++ configs/nvim/after/plugin/lsp.lua
@@ -1,62 +1,45 @@
 require("mason").setup {}
+require("mason-lspconfig").setup {}
+require("rust-tools").setup {}
 
-local lspconfig = require 'lspconfig'
-local masonlsp = require("mason-lspconfig")
-masonlsp.setup()
-masonlsp.setup_handlers {
-  function (server_name) -- Default handler
-    lspconfig[server_name].setup {}
-  end,
-  ["lua_ls"] = function ()
-    lspconfig["lua_ls"].setup {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
+vim.lsp.config.lua_ls = {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
       },
-    }
-  end,
-  ["rust_analyzer"] = function ()
-    require("rust-tools").setup {}
-  end,
-  ["pylsp"] = function ()
-    lspconfig["pylsp"].setup({
-      settings = {
-        pylsp = {
-          plugins = {
-            pycodestyle = { enabled = false },
-          },
-        },
-        python = {
-          analysis = {
-            -- Ignore all files for analysis to exclusively use Ruff for linting
-            ignore = { '*' },
-          },
-        },
+    },
+  },
+}
+vim.lsp.config.pylsp = {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = false },
       },
-    })
-  end,
-  ["ruff_lsp"] = function ()
-    lspconfig["ruff_lsp"].setup {
-      init_options = {
-        settings = {
-          args = {},
-        }
-      }
-    }
-  end,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
 }
 
--- setup language servers here
--- TODO: add a check that mason does not already have a handler for the server
-lspconfig.clangd.setup {}
-lspconfig.ocamllsp.setup({
-    cmd = { "ocamllsp" },
-    filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
-    root_dir = lspconfig.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
-})
+vim.lsp.config.ruff = {
+  init_options = {
+    settings = {
+      args = {},
+    }
+  }
+}
+
+vim.lsp.config.ocamllsp = {
+  cmd = { "ocamllsp" },
+  filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+  root_markers = { "*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace" }
+}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -74,4 +57,3 @@ vim.keymap.set('', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>')
 vim.keymap.set('', '<leader>ch', ':lua vim.lsp.buf.signature_help()<CR>')
 vim.keymap.set('', '<leader>cf', ':lua vim.lsp.buf.format()<CR>')
 vim.keymap.set('', '<leader>cr', ':lua vim.lsp.buf.rename()<CR>')
-
